096a04a31fd303380e1f993d188282104baf82e4
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-09-16 11:16:04 -0500

    Adding a new user

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { FloatingLabel } from 'react-bootstrap';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
 
-function RegisterForm({ user, updateUser }) {
+function RegisterForm({ user, onUpdate }) {
   const [formData, setFormData] = useState({
     bio: '',
     uid: user.uid,
@@ -12,15 +13,42 @@ function RegisterForm({ user, updateUser }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    registerUser(formData).then(() => updateUser(user.uid));
+    const payload = {
+      ...formData, UID: user.uid, ProfileImageURL: user.fbUser.photoURL, CreatedOn: new Date(Date.now()), Active: true,
+    };
+    console.warn('my payload', payload);
+    registerUser(payload);
+    onUpdate();
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>First Name</Form.Label>
+        <Form.Control as="textarea" name="FirstName" required placeholder="First Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control as="textarea" name="LastName" required placeholder="Last Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Label>Email</Form.Label>
+        <Form.Control as="textarea" name="Email" required placeholder="Email" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Label>Bio</Form.Label>
+        <Form.Control as="textarea" name="Bio" required placeholder="Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Label> Staff member?  </Form.Label>
+        <FloatingLabel controlId="floatingInput1" label="Are you a Staff?" className="mb-3" style={{ color: 'red' }}>
+          <Form.Select
+            type="text"
+            placeholder=""
+            name="isStaff"
+            onChange={({ target }) => {
+              const selectedValue = target.value === 'true'; // Convert to boolean
+              setFormData((prev) => ({ ...prev, [target.name]: selectedValue }));
+            }}
+            required
+          >
+            <option>Are you a staff?</option>
+            <option value="true" style={{ color: 'black' }}>Yes</option>
+            <option value="false" style={{ color: 'black' }}>No</option>
+          </Form.Select>
+        </FloatingLabel>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
@@ -32,8 +60,11 @@ function RegisterForm({ user, updateUser }) {
 RegisterForm.propTypes = {
   user: PropTypes.shape({
     uid: PropTypes.string.isRequired,
+    fbUser: PropTypes.shape({
+      photoURL: PropTypes.string.isRequired,
+    }).isRequired,
   }).isRequired,
-  updateUser: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default RegisterForm;
