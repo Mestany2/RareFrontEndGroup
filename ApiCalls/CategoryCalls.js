a6d6f9c891b353dd6b9cd261bec30ce848b2560f
Cole Amantea <cole.ama@gmail.com>
2023-09-18 21:20:50 -0500

    create post

---
+++ ApiCalls/CategoryCalls.js
@@ -0,0 +1,18 @@
+const getAllCategories = () => new Promise((resolve, reject) => {
+  fetch('https://localhost:7100/api/Categories', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+export { getAllCategories };
---
+++ ApiCalls/PostCalls.js
@@ -0,0 +1,38 @@
+const createPost = (payload) => new Promise((resolve, reject) => {
+  fetch('https://localhost:7100/api/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+const updatePost = (payload) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7100/api/posts${payload.id}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+export { updatePost, createPost };
